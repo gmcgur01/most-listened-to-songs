78cb54331e18a845d86c676533b1955c4384084e
gmcgur01 <George.McGurkin@tufts.edu>
2024-11-25 00:38:11 -0500

    client data

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+
+def main():
+    load_dotenv()
+    
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+
+    
+
+
+    
+
+if __name__ == "__main__":
+    main()
