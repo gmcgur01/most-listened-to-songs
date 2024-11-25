74bbdb488e99c64727b83f27f8578cb118bfce16
gmcgur01 <George.McGurkin@tufts.edu>
2024-11-25 02:00:24 -0500

    redirect working

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-import requests
 from dotenv import load_dotenv
 import os
+from fastapi import FastAPI
+import uvicorn
+from fastapi.responses import RedirectResponse
+from urllib.parse import urlencode
 
+load_dotenv()
 
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+app = FastAPI()
+
+@app.get("/")
 def main():
-    load_dotenv()
-    
-    client_id = os.getenv("CLIENT_ID")
-    client_secret = os.getenv("CLIENT_SECRET")
+    return redirect_auth_url()
 
-    
+@app.get("/redirect")
+def redirect(code: str = "", state: str = ""):
+    return {"code" : code}
 
+def redirect_auth_url():
+    response_type = "code"
+    redirect_uri = "http://127.0.0.1:3000/redirect"
+    scope = ""
 
-    
+    params = {
+        "client_id" : client_id,
+        "response_type" : response_type,
+        "redirect_uri" : redirect_uri,
+        "scope" : scope,
+    }
+
+    return RedirectResponse("https://accounts.spotify.com/authorize?" + urlencode(params))
+
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", port=3000, log_level="info")
